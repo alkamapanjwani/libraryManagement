d4b86306f7d4a4ccbba588411705239f2cd2eb82
alkamarajan <42032427+alkamarajan@users.noreply.github.com>
2023-03-14 19:30:20 +0530

    remove debug

--- app.py
+++ app.py
@@ -60,8 +60,8 @@ app.register_blueprint(transaction_bp)
 #     return render_template('member.html',member=data)
 
 
-if __name__ == "__main__":
-    app.run(debug=True)
+# if __name__ == "__main__":
+#     app.run(debug=True)
 
 # @app.route("/name/<name>/")
 # def hello_world(name):
